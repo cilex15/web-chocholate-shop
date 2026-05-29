1757393dd2666d5ad6ec0f31ebbbf80c55fc09ed
Sergej Ćišić <cisicsergej123@gmail.com>
2026-05-29 22:22:57 +0200

    Initial Spring Boot MVC setup

---
+++ web-chocholate-shop/src/main/java/com/sergej/web_chocholate_shop/WebChocholateShopApplication.java
@@ -0,0 +1,13 @@
+package com.sergej.web_chocholate_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChocholateShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChocholateShopApplication.class, args);
+	}
+
+}
